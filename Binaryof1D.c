00c95d030dc02fea20a989910dbf0f27048a3de7
Vamshi Teja Kumbham <112389830+VamshiTejaKumbham@users.noreply.github.com>
2023-10-04 21:03:37 +0530

    Add files via upload

---
+++ Binaryof1D.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void main(){
+    int a[50],n,loc=-1,key,beg,last,mid,i;
+    printf("Enter the size of the array : ");
+    scanf("%d",&n);
+    printf("Enter array elements : ");
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    beg=0;
+    last=n-1;
+    printf("Enter integer value to search in sorted array : ");
+    scanf("%d",&key);
+    while(beg<=last){
+
+        mid = (beg+last)/2;
+        if(a[mid]==key){
+            loc=mid;
+            break;
+        }
+        else if(a[mid]>key){
+            last = mid-1;
+        }
+        else if(a[mid]<key){
+            beg = mid+1;
+        }
+    }
+    if(loc!=-1){
+        printf("Element found at %d",loc+1);
+    }
+    else{
+        printf("Element not found");
+    }
+}
